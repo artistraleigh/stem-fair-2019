3395a19bc6008decd8efc6d5a88c207686c8cd01
Raleigh M <toraleighmartin@gmail.com>
2019-10-05 16:59:41 -0600

    add images and app

---
+++ fireside-quilt-maker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Fireside Quilt Maker</h1>
+      <h2>Make Your Block</h2>
+    </div>
+  );
+}
+
+export default App;
