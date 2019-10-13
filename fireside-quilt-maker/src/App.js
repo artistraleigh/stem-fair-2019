ab62593741fce685c06dfbcd1785edc4b0f76390
Raleigh M <toraleighmartin@gmail.com>
2019-10-13 15:00:16 -0600

    remove manifest and add pictures array

--- fireside-quilt-maker/src/App.js
+++ fireside-quilt-maker/src/App.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const colors = ['red', 'orange', 'gold', 'limegreen', 'lightseagreen', 'blueviolet', 'pink'];
+  console.log(colors);
+  const pictures = ['butterfly', 'dinosaur', 'mercat', 'rocket', 'soccer', 'unicorn'];
+  console.log(pictures);
   return (
     <div>
       <h1>Fireside Quilt Maker</h1>
       <h2>Make Your Block</h2>
+      <label htmlFor="firstName">
+        First Name
+        <input name="firstName" />
+      </label>
+      <label htmlFor="lastName">
+        Last Name
+        <input name="lastName" />
+      </label>
     </div>
   );
 }
